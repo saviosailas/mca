70279cc12cd1e5a90cf703fdc87988fc89a240ba
savio sailas <30656522+saviosailas@users.noreply.github.com>
2024-10-20 12:27:48 +0530

    09 screenshot

--- DAA/09_matrix_chain_multiplication.c
+++ DAA/09_matrix_chain_multiplication.c
@@ -1,19 +1,18 @@
 // Matrix chain multiplication and how to place the parantheses
 
-#include<stdio.h>
-#include<limits.h>
-
+#include <stdio.h>
+#include <limits.h>
 #define infinity 999999999
-
 long int m[20][20];
 int s[20][20];
 int d[20], i, j, n;
 
 void print_optimal(int i, int j)
 {
-    if (i==j)
+    if (i == j)
         printf("A%d", i);
-    else{
+    else
+    {
         printf("(");
         print_optimal(i, s[i][j]);
         print_optimal(s[i][j] + 1, j);
@@ -25,18 +24,18 @@ void matmultiply(void)
 {
     long int q;
     int k;
-    for (i=n; i>0; i--)
+    for (i = n; i > 0; i--)
     {
-        for (j=i; j<=n; j++)
+        for (j = i; j <= n; j++)
         {
-            if(i==j)
+            if (i == j)
                 m[i][j] = 0;
             else
             {
-                for(k=i; k<j; k++)
+                for (k = i; k < j; k++)
                 {
-                    q=m[i][k]+m[k+1][j];
-                    if(q<m[i][j])
+                    q = m[i][k] + m[k + 1][j] + d[i - 1] * d[k] * d[j];
+                    if (q < m[i][j])
                     {
                         m[i][j] = q;
                         s[i][j] = k;
@@ -46,61 +45,62 @@ void matmultiply(void)
         }
     }
 }
-
 int MatrixChainOrder(int d[], int i, int j)
 {
-    if( i== j)
+    if (i == j)
         return 0;
     int k;
     int min = INT_MAX;
     int count;
-    for (k=i; k<j; k++)
+    for (k = i; k < j; k++)
     {
-        count = MatrixChainOrder(d, i, k) + MatrixChainOrder(d, k+1, j + d[i-1]*d[j]);
+        count = MatrixChainOrder(d, i, k) +
+                MatrixChainOrder(d, k + 1, j) + d[i - 1] * d[k] * d[j];
         if (count < min)
             min = count;
     }
     // Return minimum count
     return min;
 }
-
-void main() 
+void main()
 {
     int k;
-    printf("\nKey in the count of matrtices\t");
+    printf("\nKey in the count of matrices\t");
     scanf("%d", &n);
-    for(i=1; i<=n; i++)
-        for(j=i+1; j<=n; j++)
+    for (i = 1; i <= n; i++)
+        for (j = i + 1; j <= n; j++)
         {
             m[i][j] = 0;
             m[i][j] = infinity;
             s[i][j] = 0;
         }
-    printf ("\n\nKey in the dimensions\n");
-    for(k=0; k<=n; k++)
+    printf("\n\nKey in the dimensions\n");
+    for (k = 0; k <= n; k++)
     {
-        printf("d%d: ", k);
+        printf("d%d:", k);
         scanf("%d", &d[k]);
     }
     matmultiply();
     printf("\nMinimum cost elements m[i][j] are\n");
-    for(i=1; i<=n; i++)
+    for (i = 1; i <= n; i++)
     {
         printf("\n\n\t");
-        for(j=i; j<=n; j++)
-            printf("m[%d][%d]: %ld\t", i, j, m[i][j]);
+        for (j = i; j <= n; j++)
+            printf("m[%d][%d]:%ld\t", i, j, m[i][j]);
     }
     // cost matrix
+
     printf("\n\nMinimum cost diagonal matrix is\n");
-    for(i=1; i<=n; i++)
+    for (i = 1; i <= n; i++)
     {
         printf("\n\n");
-        for(j=1; j<=n; j++)
+        for (j = 1; j <= n; j++)
             printf("\t%ld", m[i][j]);
     }
-    printf("\n\nMinimum number of multiplications is : %d", MatrixChainOrder(d,1,n));
-    i=1, j=n;
-    printf("\nMultiplication sequence for the %d matrices is", n);
+    printf("\n\n Minimum number of multiplications is : %d", MatrixChainOrder(d, 1, n));
+    printf("\n\n");
+    i = 1, j = n;
+    printf("\n Multiplication Sequence for the %d matrices is ", n);
     print_optimal(i, j);
     printf("\n\n");
 }
